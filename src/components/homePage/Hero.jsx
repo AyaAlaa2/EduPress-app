87ad7c082bb5ef989b48037364bf450214f22c21
Aya Alaa <aya-28-10@hotmail.com>
2025-12-26 11:59:51 +0200

    update code

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Button } from "../ui/button";
+import img1 from "./HeroSVG/img1.svg";
+import img2 from "./HeroSVG/img2.svg";
 
 const Hero = () => {
   return (
-    <div className="bg-[url('/background.svg')] h-[90vh] md:h-[100vh] bg-cover bg-center flex items-center justify-center md:justify-start ps-0 md:ps-25">
-      <div className="w-120 inset-0 bg-opacity-50 flex flex-col justify-center items-center md:items-start gap-10">
+    <div className="relative bg-linear-to-r from-[#FFF5BE] to-[#D0F7EA] md:bg-[url('/background.svg')] h-[90vh] md:h-[100vh] bg-cover bg-center flex items-center justify-center md:justify-start ps-0 md:ps-25 overflow-hidden">
+      <img src={img1} className="absolute md:hidden bottom-0 left-0 w-120 " />
+      <img src={img2} className="absolute md:hidden top-0 right-0 w-120 " />
+      <div className="absolute md:hidden top-0 left-0 w-full h-full bg-[#F0F0F0]/10 z-0"></div>
+
+      <div className="absolute md:relative  w-120 inset-0 bg-opacity-50 flex flex-col justify-center items-center md:items-start gap-10 px-1">
         <div>
           <p className="text-center md:text-start font-semibold text-[40px] md:text-[48px] font-[Exo] leading-[150%]">
             Build Skills with Online Course
